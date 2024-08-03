2dc890b845e3c33f536838d3ea680329c62fe344
Sim <simeon@twolivesleft.com>
2024-08-03 23:03:42 +0930

    Restores imgui config

--- 3rdparty/dear-imgui/imconfig.h
+++ 3rdparty/dear-imgui/imconfig.h
@@ -6,10 +6,12 @@
 
 #pragma once
 
-#define IMGUI_DEFINE_MATH_OPERATORS
-#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
-#define IMGUI_DISABLE_OBSOLETE_KEYIO
-#define IMGUI_DISABLE_DEFAULT_ALLOCATORS
+// TODO: John, seems like we need to add this define to this file
+ #define IMGUI_DEFINE_MATH_OPERATORS
+ // TODO: John, disabled these, but you'll want to check the comments about KeyMap in imgui.h
+ //#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+ //#define IMGUI_DISABLE_OBSOLETE_KEYIO
+ #define IMGUI_DISABLE_DEFAULT_ALLOCATORS
 
 //---- Define your own ImVector<> type if you don't want to use the provided implementation defined in imgui.h
 //#include <vector>
